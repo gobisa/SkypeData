ff5052d97f04b34b0cb2b7f686646c3cec1d1bd4
Andrew Gobis <gobisa@umich.edu>
2017-08-21 21:27:45 -0400

    fixed integer division issue with punctuation per message

--- SkypeData.cpp
+++ SkypeData.cpp
@@ -292,15 +292,17 @@ void SkypeUser::outputData(std::ofstream& output_file) {
 		output_file << name << "," << num_posts << "," << num_edits << "," << "undef." << "," << word_count << "," << vocab_size << ",";
 	}
 	
-	output_file << punctuation_count << "," << punctuation_count / num_posts << "," << link_count << ",";
+	output_file << punctuation_count << "," << punctuation_count / (double) num_posts << "," << link_count << ",";
 	output_file << link_count / (double) num_posts << "," << skype_emoji_count << "," << skype_emoji_count / (double) num_posts << ",";
 	output_file << negative_words_count << "," << positive_words_count << "," << bad_words_count << ",";
 	output_file << negative_message_count << "," << positive_message_count << "," << bad_word_message_count << ",";
 	output_file << negative_message_count / (double) num_posts << "," << positive_message_count / (double) num_posts << "," << bad_word_message_count / (double) num_posts << ",";
+	output_file << "\"";
 	for (const std::pair<string, int>& p : getTop3FromMap(emoji_count)) {
-		output_file << "\"" << p.first << " (" << p.second << ") ";
+		output_file << p.first << " (" << p.second << ") ";
 	}
 	output_file << "\",";
+	output_file << "\"";
 	for (const std::pair<string, int>& p : getTop3FromMap(vocabulary_count)) {
 		output_file << "\"" << p.first << " (" << p.second << ") ";
 	}
